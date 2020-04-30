9c464b6ca6f9123238c06aa2b21a4cb025c01801
Adriane Justine Tan <eidoriantan@gmail.com>
2020-04-30 22:52:55 +0800

    perf: Add constructor

--- src/kanji.mjs
+++ src/kanji.mjs
@@ -3,4 +3,9 @@ import dictionary from './kanjidic2/kanjidic2.min.mjs'
 
 export default class Kanji {
   static dump () { return dictionary }
+
+  constructor () {
+    this.name = 'Kanji'
+    this.version = '0.1.0'
+  }
 }
