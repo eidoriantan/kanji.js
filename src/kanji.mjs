8f1187b28364b72ab58f7dd773a5c4eb81041059
Adriane Justine Tan <eidoriantan@gmail.com>
2020-04-30 23:05:02 +0800

    feat: Get details of a character

--- src/kanji.mjs
+++ src/kanji.mjs
@@ -4,6 +4,15 @@ import dictionary from './kanjidic2/kanjidic2.min.mjs'
 export default class Kanji {
   static dump () { return dictionary }
 
+  static getDetails (character) {
+    let details = null
+    dictionary.forEach(word => {
+      if (word.literal === character) details = word
+    })
+
+    return details
+  }
+
   constructor () {
     this.name = 'Kanji'
     this.version = '0.1.0'
