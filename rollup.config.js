0874d2478b5047be305fd3f12afd838645ec2744
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-02 15:02:07 +0800

    build: Do not minify when in dev

--- rollup.config.js
+++ rollup.config.js
@@ -3,24 +3,26 @@ import { terser } from 'rollup-plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 
-export default {
-  input: 'src/kanji.mjs',
-  strictDeprecations: true,
-  output: [{
-    file: 'dist/kanji.js',
-    format: 'umd',
-    name: 'Kanji'
-  }, {
+const production = process.env.NODE_ENV === 'production'
+const outputs = [{
+  file: 'dist/kanji.js',
+  format: 'umd',
+  name: 'Kanji'
+}]
+
+if (production) {
+  outputs.push({
     file: 'dist/kanji.min.js',
     format: 'umd',
     name: 'Kanji',
     plugins: terser()
-  }],
-  plugins: [
-    commonjs(),
-    resolve()
-  ],
-  watch: {
-    include: ['src/**/*.mjs']
-  }
+  })
+}
+
+export default {
+  input: 'src/kanji.mjs',
+  strictDeprecations: true,
+  output: outputs,
+  plugins: [commonjs(), resolve()],
+  watch: { include: ['src/**/*.mjs'] }
 }
