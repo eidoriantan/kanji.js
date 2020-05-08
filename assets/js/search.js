ef28e54be9fa83b5aec00e3a6c1177414d05c927
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-08 15:40:54 +0800

    fix: Fix API and searches

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,47 +1,56 @@
 
-const api = 'https://api.kanji.js.org'
+const api = 'https://kanji-api.herokuapp.com'
+
+function searchByKanji (q, template) {
+  $.ajax(api + '/kanji/' + q, {
+    dataType: 'json',
+    method: 'GET',
+    success: function (data, status, request) {
+      $('#results').empty()
+      $('#results').append(fillResult(template, data))
+      $('#blankslate').removeClass('my-6 my-md-12')
+      $('#title').remove()
+      $('#results').removeClass('d-none')
+    },
+    error: function (request, status, error) {
+      console.log(error)
+    }
+  })
+}
+
+function searchByMeaning (q, template) {
+  q = encodeURIComponent(q)
+  $.ajax(api + '/v1/search/?meaning=' + q, {
+    dataType: 'json',
+    method: 'GET',
+    success: function (data, status, request) {
+      $('#results').empty()
+      for (let i = 0; i < data.search_results.length; i++) {
+        const word = data.search_results[i]
+        $('#results').append(fillResult(template, word))
+      }
+
+      $('#blankslate').removeClass('my-6 my-md-12')
+      $('#title').remove()
+      $('#results').removeClass('d-none')
+    },
+    error: function (request, status, error) {
+      console.log(error)
+    }
+  })
+}
 
 $(document).ready(function () {
   const template = $('#result-item-template').prop('content')
-
   $('#search-form').submit(function (event) {
     event.preventDefault()
     const q = $('#q').val()
     const charCode = q.charCodeAt(0)
 
     if (q.length === 1 && charCode > 0x4e00 && charCode < 0x9fbf) {
-      $.ajax(api + '/kanji/' + q, {
-        dataType: 'json',
-        method: 'GET',
-        success: function (data, status, request) {
-          $('#results').empty()
-          $('#results').append(fillResult(template, data))
-          animateResults()
-        },
-        error: function (request, status, error) {
-          console.log(error)
-        }
-      })
+      searchByKanji(q, template)
     } else {
-      $.ajax(api + '/v1/search/', {
-        data: { meaning: q },
-        dataType: 'json',
-        method: 'GET',
-        success: function (data, status, request) {
-          $('#results').empty()
-          for (let i = 0; i < data.search_results.length; i++) {
-            const word = data.search_results[i]
-            $('#results').append(fillResult(template, word))
-          }
-
-          $('#blankslate').removeClass('my-6 my-md-12')
-          $('#title').remove()
-          $('#results').removeClass('d-none')
-        },
-        error: function (request, status, error) {
-          console.log(error)
-        }
-      })
+      searchByMeaning(q, template)
     }
   })
 })
