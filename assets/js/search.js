386bd18f70add5511c6ebe8e7e38551a744387a0
Adriane Justine Tan <eidoriantan@gmail.com>
2020-05-15 17:01:34 +0800

    feat: Add loading dot when loading

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,24 +1,21 @@
 
 const api = 'https://kanji-api.herokuapp.com'
+let template
 
-function searchByKanji (q, template) {
+function searchByKanji (q) {
   $.ajax(api + '/kanji/' + q, {
     dataType: 'json',
     method: 'GET',
     success: function (data, status, request) {
       $('#results').empty()
-      $('#results').append(fillResult(template, data))
+      $('#results').append(getItem(data))
       $('#blankslate').removeClass('my-6 my-md-12')
       $('#title').remove()
-      $('#results').removeClass('d-none')
-    },
-    error: function (request, status, error) {
-      console.log(error)
     }
   })
 }
 
-function searchByMeaning (q, template) {
+function searchByMeaning (q) {
   q = encodeURIComponent(q)
   $.ajax(api + '/v1/search/?meaning=' + q, {
     dataType: 'json',
@@ -27,35 +24,36 @@ function searchByMeaning (q, template) {
       $('#results').empty()
       for (let i = 0; i < data.search_results.length; i++) {
         const word = data.search_results[i]
-        $('#results').append(fillResult(template, word))
+        $('#results').append(getItem(word))
       }
 
       $('#blankslate').removeClass('my-6 my-md-12')
       $('#title').remove()
-      $('#results').removeClass('d-none')
-    },
-    error: function (request, status, error) {
-      console.log(error)
     }
   })
 }
 
 $(document).ready(function () {
-  const template = $('#result-item-template').prop('content')
+  template = $('#result-item-template').prop('content')
+  const loader = $('#loader-template').prop('content')
+
   $('#search-form').submit(function (event) {
     event.preventDefault()
+    const loaderClone = $(loader).clone(true, true)
+    $('#results').html(loaderClone)
+
     const q = $('#q').val()
     const charCode = q.charCodeAt(0)
 
     if (q.length === 1 && charCode > 0x4e00 && charCode < 0x9fbf) {
-      searchByKanji(q, template)
+      searchByKanji(q)
     } else {
-      searchByMeaning(q, template)
+      searchByMeaning(q)
     }
   })
 })
 
-function fillResult (template, data) {
+function getItem (data) {
   const resultItem = $(template).clone(true, true)
   $(resultItem).find('[data-temp="character"]').text(data.literal)
   $(resultItem).find('[data-temp="meanings"]').text(data.meanings.join(', '))
