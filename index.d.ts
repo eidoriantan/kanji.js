cd2ffdcc6c0bd49ae09dd96124f8db733171f42e
Adriane Justine Geneblaza Tan <eidoriantan@gmail.com>
2022-10-28 01:23:58 +0800

    feat: add typescript support

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+
+export as namespace Kanji;
+
+export default class Kanji {
+  name: string;
+  version: string;
+  constructor ();
+
+  static dump () : Array<Kanji.Word>;
+  static getDetails (character: string) : Kanji.Word;
+  static search (options?: Kanji.SearchOptions) : Array<Kanji.Word>;
+  static random () : Kanji.Word;
+}
+
+export interface Word {
+  freq: number;
+  grade: number;
+  jlpt: number;
+  kunyomi: Array<string>;
+  literal: string;
+  meanings: Array<string>;
+  onyomi: Array<string>;
+  stroke_count: number;
+}
+
+export interface SearchOptions {
+  grade?: Kanji.RangeOption | number;
+  jlpt?: Kanji.RangeOption | number;
+  meaning?: string;
+  romaji?: string;
+  sort?: boolean;
+}
+
+export interface RangeOption {
+  min?: number;
+  max?: number;
+}
